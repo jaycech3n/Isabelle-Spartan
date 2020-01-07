695e461de0ecebdb8a52537ff7d758c11cb14f40
Josh Chen <josh@joshchen.io>
2020-01-07 20:40:07 +1300

    pre-branch

--- Identity.thy
+++ Identity.thy
@@ -51,45 +51,60 @@ text \<open>
 \<close>
 
 schematic_goal pathcomp_assoc:
-  assumes
-    "p: x =\<^bsub>A\<^esub> y" "q: y =\<^bsub>A\<^esub> z" "r: z =\<^bsub>A\<^esub> w"
-    "A: U i" "x: A" "y: A" "z: A" "w: A"
-  shows "?prf:
-    pathcomp A x y w p (pathcomp A y z w q r) =\<^bsub>x =\<^bsub>A\<^esub> w\<^esub>
-      pathcomp A x z w (pathcomp A x y z p q) r"
-apply (equality \<open>p: _\<close>)
-  apply ((rule Id_transitive; routine?)+) [2]
-  schematic_subgoal premises for x q
-    apply (equality \<open>q: _\<close>)
-      apply ((rule Id_transitive; routine?)+) [2]
-      schematic_subgoal premises for x r
-        apply (equality \<open>r: _\<close>)
-          apply ((rule Id_transitive; routine?)+) [2]
-          apply (reduce+; routine)
-      done
+assumes
+  "p: x =\<^bsub>A\<^esub> y" "q: y =\<^bsub>A\<^esub> z" "r: z =\<^bsub>A\<^esub> w"
+  "A: U i" "x: A" "y: A" "z: A" "w: A"
+shows "?prf:
+  pathcomp A x y w p (pathcomp A y z w q r) =\<^bsub>x =\<^bsub>A\<^esub> w\<^esub>
+    pathcomp A x z w (pathcomp A x y z p q) r"
+  apply (equality \<open>p: _\<close>)
+    apply ((rule Id_transitive; routine?)+) [2]
+    schematic_subgoal premises for x q
+      apply (equality \<open>q: _\<close>)
+        apply ((rule Id_transitive; routine?)+) [2]
+        schematic_subgoal premises for x r
+          apply (equality \<open>r: _\<close>)
+            apply ((rule Id_transitive; routine?)+) [2]
+            apply (reduce+; routine)
+        done
+    done
   done
-done
 
 schematic_goal Id_transfer_derivation:
-  assumes
-    "x: A" "y: A" "p: x =\<^bsub>A\<^esub> y"
-    "f: A \<rightarrow> B" "A: U i" "B: U i"
-  shows "?prf: f `x =\<^bsub>B\<^esub> f `y"
+assumes
+  "x: A" "y: A" "p: x =\<^bsub>A\<^esub> y"
+  "f: A \<rightarrow> B" "A: U i" "B: U i"
+shows "?prf: f `x =\<^bsub>B\<^esub> f `y"
   by (equality \<open>p: _\<close>) routine
 
 definition "ap f A B x y p \<equiv> IdInd A (\<lambda>x y _. f `x =\<^bsub>B\<^esub> f `y) (\<lambda>x. refl (f `x)) x y p"
 
 lemma Id_transfer:
-  assumes
-    "p: x =\<^bsub>A\<^esub> y" "x: A" "y: A"
-    "f: A \<rightarrow> B" "A: U i" "B: U i"
-  shows "ap f A B x y p: f `x =\<^bsub>B\<^esub> f `y"
+assumes
+  "p: x =\<^bsub>A\<^esub> y" "x: A" "y: A"
+  "f: A \<rightarrow> B" "A: U i" "B: U i"
+shows "ap f A B x y p: f `x =\<^bsub>B\<^esub> f `y"
   unfolding ap_def by (rule Id_transfer_derivation assms)+
 
 lemma ap_comp [reds]:
-  assumes "f: A \<rightarrow> B" "A: U i" "B: U i" "x: A"
-  shows "ap f A B x x (refl x) \<equiv> refl (f `x)"
+assumes "f: A \<rightarrow> B" "A: U i" "B: U i" "x: A"
+shows "ap f A B x x (refl x) \<equiv> refl (f `x)"
   unfolding ap_def by (reduce | routine)+
+
+(* Okay look, this is very ugly and inconvenient. *)
+schematic_goal ap_pathcomp_derivation:
+assumes
+  "A: U i" "B: U i"
+  "f: A \<rightarrow> B"
+  "x: A" "y: A" "z: A"
+  "p: x =\<^bsub>A\<^esub> y" "q: y =\<^bsub>A\<^esub> z"
+shows
+  (* What we want to write: "ap f (p \<bullet> q) = (ap f p) \<bullet> (ap f q)" *)
+  "?prf: ap f A B x z (pathcomp A x y z p q) =\<^bsub>(f `x) =\<^bsub>B\<^esub> (f `z)\<^esub>
+    pathcomp B (f `x) (f` y) (f `z) (ap f A B x y p) (ap f A B y z q)"
+  apply (equality \<open>p: _\<close>)
+    apply (routine)
+oops
 
 
 end
