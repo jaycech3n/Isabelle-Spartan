211bb295c830b96523ad14d9d8f743d21cb6b974
Josh Chen <josh@joshchen.io>
2020-02-09 21:30:49 +0100

    1. Reformulate properties of homotopy. 2. Prove homotopy_funcomp. 3. Prove biinv_imp_qinv.

--- Equivalence.thy
+++ Equivalence.thy
@@ -38,21 +38,16 @@ schematic_goal homotopy_symmetric_derivation:
     "\<And>x. x: A \<Longrightarrow> B x: U i"
     "f: \<Prod>x: A. B x"
     "g: \<Prod>x: A. B x"
-  shows "?prf: f ~ g \<rightarrow> g ~ f"
-  apply intro
+  shows "H: f ~ g \<Longrightarrow> ?prf: g ~ f"
   unfolding homotopy_def
-    \<guillemotright> for H
-      apply intros
-        apply (rule Id_symmetric)
-          \<^item> for x
-            apply (rule PiE[of H _ _ x])
-            done
-          \<^item> by typechk
-      done
+  apply intros
+    apply (rule Id_symmetric)
+      \<guillemotright> for x by (rule PiE[of H _ _ x])
+      \<guillemotright> by typechk
   done
 
-definition "homotopy_symmetric A B f g \<equiv>
-  \<lambda>H: homotopy A (\<lambda>x. B x) f g. \<lambda>x: A. pathinv (B x) (f `x) (g `x) (H `x)"
+definition "homotopy_symmetric A B f g H \<equiv>
+  \<lambda>x: A. pathinv (B x) (f `x) (g `x) (H `x)"
 
 lemmas homotopy_symmetric [typechk] =
   homotopy_symmetric_derivation [folded homotopy_symmetric_def]
@@ -64,23 +59,19 @@ schematic_goal homotopy_transitive_derivation:
     "f: \<Prod>x: A. B x"
     "g: \<Prod>x: A. B x"
     "h: \<Prod>x: A. B x"
-  shows "?prf: f ~ g \<rightarrow> g ~ h \<rightarrow> f ~ h"
-  apply intros
+  shows "\<lbrakk>H1: f ~ g; H2: g ~ h\<rbrakk> \<Longrightarrow> ?prf: f ~ h"
   unfolding homotopy_def
-    \<guillemotright> for H1 H2 apply intro
-      \<^item> for x
-        apply (rule Id_transitive[where ?y = "g `x"])
-          \<^enum> by (rule PiE[of H1 _ _ x])
-          \<^enum> by (rule PiE[of H2 _ _ x])
-        done
-      \<^item> by typechk
+  apply intro
+    \<guillemotright> for x
+      apply (rule Id_transitive[where ?y = "g `x"])
+        \<^item> by (rule PiE[of H1 _ _ x])
+        \<^item> by (rule PiE[of H2 _ _ x])
       done
+    \<guillemotright> by typechk
   done
 
-definition "homotopy_transitive A B f g h \<equiv>
-  \<lambda>H1: homotopy A (\<lambda>x. B x) f g.
-    \<lambda>H2: homotopy A (\<lambda>x. B x) g h.
-      \<lambda>x: A. pathcomp (B x) (f `x) (g `x) (h `x) (H1 `x) (H2 `x)"
+definition "homotopy_transitive A B f g h H1 H2 \<equiv>
+  \<lambda>x: A. pathcomp (B x) (f `x) (g `x) (h `x) (H1 `x) (H2 `x)"
 
 lemmas homotopy_transitive [typechk] =
   homotopy_transitive_derivation [folded homotopy_transitive_def]
@@ -125,8 +116,39 @@ schematic_goal homotopy_id_right [typechk]:
   shows "homotopy_refl A f: f \<circ> (id A) ~ f"
   unfolding homotopy_refl_def homotopy_def by (subst comps) typechk
 
+schematic_goal homotopy_funcomp_left:
+  assumes
+    "A: U i" "B: U i"
+    "\<And>x. x: B \<Longrightarrow> C x: U i"
+    "f: A \<rightarrow> B"
+    "g: \<Prod>x: B. C x"
+    "g': \<Prod>x: B. C x"
+    "H: homotopy B C g g'"
+  shows "?prf: homotopy A {} (g \<circ>\<^bsub>A\<^esub> f) (g' \<circ>\<^bsub>A\<^esub> f)"
+  unfolding homotopy_def
+  apply (intro; reduce)
+    apply (insert \<open>H: _\<close>[unfolded homotopy_def])
+      apply (erule PiE[of H]; typechk)
+  done
 
-section \<open>Quasi-inverse\<close>
+schematic_goal homotopy_funcomp_right:
+  assumes
+    "A: U i" "B: U i" "C: U i"
+    "f: A \<rightarrow> B"
+    "f': A \<rightarrow> B"
+    "g: B \<rightarrow> C"
+    "H: homotopy A (\<lambda>_. B) f f'"
+  shows "?prf: homotopy A {} (g \<circ>\<^bsub>A\<^esub> f) (g \<circ>\<^bsub>A\<^esub> f')"
+  unfolding homotopy_def
+  apply (intro; reduce)
+    apply (insert \<open>H: _\<close>[unfolded homotopy_def]; drule PiE, assumption)
+      apply (rule Id_transfer, assumption)
+  done
+
+
+section \<open>Quasi-inverse and bi-invertibility\<close>
+
+subsection \<open>Quasi-inverses\<close>
 
 definition "qinv A B f \<equiv> \<Sum>g: B \<rightarrow> A.
   homotopy A (\<lambda>_. A) (g \<circ>\<^bsub>A\<^esub> f) (id A) \<times> homotopy B (\<lambda>_. B) (f \<circ>\<^bsub>B\<^esub> g) (id B)"
@@ -199,17 +221,11 @@ definition "quasiinv_qinv A B f prf \<equiv>
 
 lemmas quasiinv_qinv = quasiinv_qinv_derivation [folded quasiinv_qinv_def]
 
-
-section \<open>Equivalence\<close>
-
-text \<open>
-  Following the HoTT book, we first define equivalence in terms of
-  bi-invertibility.
-\<close>
+subsection \<open>Bi-invertible maps\<close>
 
 definition "biinv A B f \<equiv>
   (\<Sum>g: B \<rightarrow> A. homotopy A (\<lambda>_. A) (g \<circ>\<^bsub>A\<^esub> f) (id A))
-  \<times> (\<Sum>g: B \<rightarrow> A. homotopy B (\<lambda>_. B) (f \<circ>\<^bsub>B\<^esub> g) (id B))"
+    \<times> (\<Sum>g: B \<rightarrow> A. homotopy B (\<lambda>_. B) (f \<circ>\<^bsub>B\<^esub> g) (id B))"
 
 lemma biinv_type [typechk]:
   assumes "A: U i" "B: U i" "f: A \<rightarrow> B"
@@ -237,6 +253,110 @@ definition "qinv_imp_biinv A B f \<equiv>
 
 lemmas qinv_imp_biinv [typechk] =
   qinv_imp_biinv_derivation [folded qinv_imp_biinv_def]
+
+text \<open>
+  Show that bi-invertible maps are quasi-inverses, as a demonstration of how to
+  work in this system.
+\<close>
+
+no_translations
+  "f x" \<leftharpoondown> "f `x"
+  "x = y" \<leftharpoondown> "x =\<^bsub>A\<^esub> y"
+  "g \<circ> f" \<leftharpoondown> "g \<circ>\<^bsub>A\<^esub> f"
+  "p\<inverse>" \<leftharpoondown> "CONST pathinv A x y p"
+  "p \<bullet> q" \<leftharpoondown> "CONST pathcomp A x y z p q"
+  "fst" \<leftharpoondown> "CONST Spartan.fst A B"
+  "snd" \<leftharpoondown> "CONST Spartan.snd A B"
+  "f[p]" \<leftharpoondown> "CONST ap A B x y f p"
+  "trans P p" \<leftharpoondown> "CONST transport A P x y p"
+  "trans_const B p" \<leftharpoondown> "CONST transport_const A B x y p"
+  "lift P p u" \<leftharpoondown> "CONST pathlift A P x y p u"
+  "apd f p" \<leftharpoondown> "CONST Identity.apd A P f x y p"
+  "f ~ g" \<leftharpoondown> "CONST homotopy A B f g"
+  "qinv f" \<leftharpoondown> "CONST Equivalence.qinv A B f"
+  "biinv f" \<leftharpoondown> "CONST Equivalence.biinv A B f"
+
+schematic_goal biinv_imp_qinv_derivation:
+  assumes "A: U i" "B: U i" "f: A \<rightarrow> B"
+  shows "?prf: biinv f \<rightarrow> qinv f"
+
+  text \<open>Split the hypothesis \<^term>\<open>biinv f\<close> into its components:\<close>
+  apply intro
+  unfolding biinv_def
+  apply (erule SigE, typechk)+
+
+  text \<open>Name the components:\<close>
+  \<guillemotright> premises for _ _ _ g H1 h H2
+  thm \<open>g:_\<close> \<open>h:_\<close> \<open>H1:_\<close> \<open>H2:_\<close>
+
+  text \<open>
+    \<^term>\<open>g\<close> is a quasi-inverse to \<^term>\<open>f\<close>, so the proof will be a triple
+    \<^term>\<open><g, <?H1, ?H2>>\<close>.
+  \<close>
+  unfolding qinv_def
+  apply intro
+    \<guillemotright> by (rule \<open>g: _\<close>)
+    \<guillemotright> apply intro
+      text \<open>The first part \<^prop>\<open>?H1: g \<circ> f ~ id A\<close> is given by \<^term>\<open>H1\<close>.\<close>
+      apply (rule \<open>H1: _\<close>)
+
+      text \<open>
+        It remains to prove \<^prop>\<open>?H2: f \<circ> g ~ id B\<close>. First show that \<open>g ~ h\<close>,
+        then the result follows from @{thm \<open>H2: f \<circ> h ~ id B\<close>}. Here a proof
+        block is used to calculate "forward".
+      \<close>
+      proof -
+        have "?\<alpha>: g ~ g \<circ> f \<circ> h"
+          apply (subst id_right[symmetric], typechk)
+          apply (rule homotopy_funcomp_right)
+          apply (rule homotopy_symmetric)
+          text \<open>Now we are done since this is known.\<close>
+          by fact
+  
+        moreover
+        have "?\<beta>: g \<circ> f \<circ> h ~ h"
+          apply (subst (2) id_left[symmetric, of h], typechk)
+          apply (subst funcomp_assoc[symmetric], typechk)
+          apply (rule homotopy_funcomp_left)
+          by fact
+  
+        ultimately
+        have "?\<gamma>: g ~ h"
+          apply (rule homotopy_transitive) defer
+          by assumption+ typechk
+  
+        then
+        have "?\<delta>: f \<circ> g ~ f \<circ> h"
+          by (rule homotopy_funcomp_right)
+  
+        thus "?H2: f \<circ> g ~ id B"
+          apply (rule homotopy_transitive) defer
+          by (assumption, rule \<open>H2:_\<close>) typechk
+      qed
+    done
+  done
+
+definition "biinv_imp_qinv A B f \<equiv>
+  \<lambda>x: Equivalence.biinv A B f. SigInd (\<Sum>g: B \<rightarrow> A. homotopy A (\<lambda>_. A) (g \<circ>\<^bsub>A\<^esub> f)
+  (id A)) (\<lambda>_. \<Sum>g: B \<rightarrow> A. homotopy B (\<lambda>_. B) (f \<circ>\<^bsub>B\<^esub> g) (id B)) (\<lambda>a.
+  Equivalence.qinv A B f) (\<lambda>uua y. SigInd (B \<rightarrow> A) (\<lambda>g. homotopy A (\<lambda>_. A) (g
+  \<circ>\<^bsub>A\<^esub> f) (id A)) (\<lambda>a. Equivalence.qinv A B f) (\<lambda>g ya. SigInd (B \<rightarrow> A) (\<lambda>g.
+  homotopy B (\<lambda>_. B) (f \<circ>\<^bsub>B\<^esub> g) (id B)) (\<lambda>a. Equivalence.qinv A B f) (\<lambda>h H2. <g,
+  <ya, homotopy_transitive B (\<lambda>x. B) (f \<circ>\<^bsub>B\<^esub> g) (f \<circ>\<^bsub>B\<^esub> h) (id B) (\<lambda>x: B. ap A B
+  (g `x) (h `x) f (homotopy_transitive B (\<lambda>x. A) g (g \<circ>\<^bsub>B\<^esub> f \<circ>\<^bsub>B\<^esub> h) h (\<lambda>x: B. ap
+  B A (id B `x) ((f \<circ>\<^bsub>B\<^esub> h) `x) g (homotopy_symmetric B (\<lambda>x. B) (f \<circ>\<^bsub>B\<^esub> h) (id B)
+  H2 `x)) (\<lambda>x: B. ya `(h `x)) `x)) H2>>) y) uua) x"
+
+lemmas biinv_imp_qinv [typechk] =
+  biinv_imp_qinv_derivation [folded biinv_imp_qinv_def]
+
+
+section \<open>Equivalence\<close>
+
+text \<open>
+  Following the HoTT book, we first define equivalence in terms of
+  bi-invertibility.
+\<close>
 
 definition equivalence (infix "\<simeq>" 110)
   where "A \<simeq> B \<equiv> \<Sum>f: A \<rightarrow> B. Equivalence.biinv A B f"
